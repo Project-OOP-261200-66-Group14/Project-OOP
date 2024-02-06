ac36d3cf05d296cd2ac3f3969c57d28f889c8b41
hghgh <apiwit_boonyarit@cmu.ac.th>
2024-02-07 00:28:14 +0700

    update

---
+++ src/IPlayer.java
@@ -0,0 +1,8 @@
+public interface IPlayer {
+
+
+    public Player getOwner();
+    public String getName();
+    public void updateBudget();
+    public long getBudget();
+}
---
+++ src/Player.java
@@ -0,0 +1,28 @@
+public class Player extends Region implements IPlayer{
+    private long budget;
+    private String name;
+
+    @Override
+    public long getBudget() {
+        return 0;
+    }
+
+    @Override
+    public void updateBudget() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Player getOwner() {
+        return null;
+    }
+
+//    public Region curRegion(){
+//
+//    }
+}
