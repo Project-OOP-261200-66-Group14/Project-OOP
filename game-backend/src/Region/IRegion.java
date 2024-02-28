fdf4cace5d016c3a4affa087ba861de59d7ca6be
hghgh <apiwit_boonyarit@cmu.ac.th>
2024-02-29 01:26:17 +0700

    update

--- game-backend/src/Region/IRegion.java
+++ game-backend/src/Region/IRegion.java
@@ -4,7 +4,12 @@ public interface IRegion {
 
     bolean isCityCenter();
 
-    long
+    long getDeposit();
+
+    void changeCityCenter();
+
+    void updateDeposit(long amount);
+    void setCityCenter(Player owner);
 
 
 
