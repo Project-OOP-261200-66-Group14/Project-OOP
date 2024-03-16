6668d164b0ed3d9d86e49d406467cc69b8e08eba
Surapa <surapa152@gmail.com>
2024-03-17 01:24:34 +0700

    Integrate Spring Initializr project into existing GitHub project

---
+++ src/main/java/group14/demo/Group14Application.java
@@ -0,0 +1,13 @@
+package group14.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group14Application.class, args);
+	}
+
+}
---
+++ src/test/java/group14/demo/Group14ApplicationTests.java
@@ -0,0 +1,13 @@
+package group14.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
